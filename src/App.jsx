b7e4eba590ddcbd04a0f957dd8b0b8fd9ae32cea
PatriceRoot <patricekalwira@gmail.com>
2024-06-06 15:18:13 +0200

    feat: delete the alert message when the user clicks the submit button

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,18 @@ import ResultatGallery from './components/ResultatGallery.jsx';
 import NavBar from './components/NavBar.jsx';
 import ArticleCard from './components/ArticleCard.jsx';
 import ExportDropdownButton from './components/DropdownExport.jsx';
-import { Menu } from './components/Menu';
-import { datab } from '../data';
 function App() {
-    const titre = 'Pays';
-    const handleClick = () => {
-        alert('Button clicked!');
-    };
+    const handleClick = () => {};
     return (
-        <div>
-            <h2>Most visited wikimedia articles</h2>
+        <div id='results'>
+            <ExportDropdownButton htmlId='results' fileName='results' />
+            <NavBar />
             <ResultatGallery />
+            <ArticleCard />
             <SetDate />
-            <Menu data={datab} title={titre} />
             <MostArticleByProject project='fr.wikipedia' />
             <Button text='submit' event={handleClick}></Button>
             <WikiAfricaTopArticles />
-            <NavBar />
-            <ArticleCard />
-            <ExportDropdownButton />
             <Footer />
         </div>
     );
